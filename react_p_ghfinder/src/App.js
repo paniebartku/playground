e0d9e038fdbcef7753d72c5485efe4fbb704bfdf
Bartłomiej Wilczek <bartlomiejwilczek@o2.pl>
2019-08-07 15:16:45 +0200

    spinner component and some refactorinf (stateless components)

--- react_p_ghfinder/src/App.js
+++ react_p_ghfinder/src/App.js
@@ -1,14 +1,35 @@
 import React, { Component } from "react";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
-import UserItem from "./components/users/UserItem";
+import Users from "./components/users/Users";
+import axios from "axios";
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  };
+  async componentDidMount() {
+    // fetch("https://api.github.com/users")
+    //   .then(res => res.json())
+    //   .then(data => console.log(data));
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/users?client_id=${
+        process.env.REACT_APP_GH_FINDER_ID
+      }&client_secret${process.env.REACT_APP_GH_FINDER_CLIENT_SECRET}`
+    );
+    // .then(res => console.log(res.data));
+    console.log(res.data);
+    this.setState({ users: res.data, loading: false });
+  }
   render() {
     return (
       <div className="App">
         <Navbar iconProp="fab fa-github" />
-        <UserItem />
+        <div className="container">
+          <Users loading={this.state.loading} users={this.state.users} />
+        </div>
       </div>
     );
   }
--- react_p_ghfinder/src/components/layout/Navbar.js
+++ react_p_ghfinder/src/components/layout/Navbar.js
@@ -1,26 +1,23 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 
-class Navbar extends Component {
-  static defaultProps = {
-    title: "GH Finder",
-    iconProp: "fab fa-github"
-  };
+const Navbar = ({ iconProp, title }) => {
+  return (
+    <nav className="navbar bg-primary">
+      <h1>
+        <i className={iconProp} />
+        {title}
+      </h1>
+    </nav>
+  );
+};
+Navbar.defaultProps = {
+  title: "GH Finder",
+  iconProp: "fab fa-github"
+};
 
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-    iconProp: PropTypes.string.isRequired
-  };
-  render() {
-    return (
-      <nav className="navbar bg-primary">
-        <h1>
-          <i className={this.props.iconProp} />
-          {this.props.title}
-        </h1>
-      </nav>
-    );
-  }
-}
-
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  iconProp: PropTypes.string.isRequired
+};
 export default Navbar;
